5db02d9715311fc7c2c78856c983228771e0f4e3
kumawatdevesh99@gmail.com <kumawatdevesh99@gmail.com>
2020-08-17 10:52:37 +0530

    added port env

--- app.js
+++ app.js
@@ -23,8 +23,7 @@ app.use('/api/tweets', tweetRouter);
 
 mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-tpp9z.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`)
 .then(res => {
-    app.listen(5000);
-    console.log('connected');
+    app.listen(process.env.PORT || 5000);
 })
 .catch(err => {
     console.log(err);
