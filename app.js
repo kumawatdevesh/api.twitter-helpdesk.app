3c590df9f88b4494eff1eac85b155efb53a4ae05
kumawatdevesh99@gmail.com <kumawatdevesh99@gmail.com>
2020-08-19 23:03:53 +0530

    modification

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.use('/api/users', userRouter);
 
 mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-tpp9z.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`)
 .then(res => {
+    console.log('connected');
     app.listen(process.env.PORT || 5000);
 })
 .catch(err => {
